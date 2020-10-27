8c8a737552f81e29e04ab976c8d14f602c7ecb8f
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-27 13:09:39 +0530

    working on client to client connect

--- tracker.cpp
+++ tracker.cpp
@@ -23,12 +23,27 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, portno;
+    // read tracker info
+    ifstream trackerInfoFile(argv[1]);
+    int trackerno = atoi(argv[2]);
+
+    string tracker_ip_addr;
+    string port_str;
+
+    for(int i=0;i<trackerno;i++)
+    {
+        getline(trackerInfoFile, tracker_ip_addr);
+        getline(trackerInfoFile, port_str);
+    }
+    int portno = atoi(port_str.c_str());
+    trackerInfoFile.close();
+
+    int sockfd, newsockfd;
     socklen_t clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    if (argc < 2)
+    if (argc < 3)
     {
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
@@ -39,8 +54,6 @@ int main(int argc, char *argv[])
         error("ERROR opening socket");
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
-
-    portno = atoi(argv[1]);
 
     serv_addr.sin_family = AF_INET;
 
@@ -62,18 +75,7 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    //vector<int> sockfds;
-    //vector<thread> allthreads;
-    //while (true)
-    //{
-    //    sockfds.push_back(accept(sockfd, (struct sockaddr *)&cli_addr, &clilen));
-    //    if (sockfds.back() < 0)
-    //        error("ERROR on accept");
-    //
-    //    printf("server: got connection from %s port %d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-    //    thread thread_obj(ThreadForClient, sockfds.back());
-    //    allthreads.push_back(move(thread_obj));
-    //}
+
     close(sockfd);
     return 0;
 }
