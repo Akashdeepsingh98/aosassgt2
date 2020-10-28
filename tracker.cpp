a3aec47b8defa2ba7993768908afa880af8514c7
Akashdeepsingh98 <ad@DESKTOP-63V9KB9.localdomain>
2020-10-28 10:21:48 +0530

    removed lines

--- tracker.cpp
+++ tracker.cpp
@@ -54,11 +54,8 @@ int main(int argc, char *argv[])
         error("ERROR opening socket");
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
-
     serv_addr.sin_family = AF_INET;
-
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-
     serv_addr.sin_port = htons(portno);
 
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
